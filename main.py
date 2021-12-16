df02bf8af6c5c3d48adca047d9fc3be4007a3df6
mkurmarc <51379562+mkurmarc@users.noreply.github.com>
2021-12-16 13:50:51 -0800

    add: get one post path operation

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -13,6 +14,17 @@ class Post(BaseModel):
     published: bool = True # default value is true if no user provides value
     rating: Optional[int] = None # fully optional field tht defaults to None
 
+
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, 
+    {"title": "favorite foods", "content": "i like pizzas", "id": 2}]
+
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
+
 @app.get("/")
 def root():
     return {"message": "Hello World!!!"}
@@ -20,7 +32,8 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return{"data":"This is your posts"}
+    return{"data": my_posts} # If I pass in an array like this, FastAPI 
+                             # serializes 'my_posts' converting it into JSON
 
 '''
 # another example of using the Body
@@ -30,9 +43,19 @@ def create_posts(payload: dict = Body(...)): # saves body content to a dict name
     return {"new_post": f"title {payload['title']} content: {payload['content']}"}
 '''
 
-@app.post("/createposts")
-def create_posts(new_post: Post): # FastAPI automatically checks frontend payload if data fits the 
-    print(new_post.published)     # schema model, Post. If true, then it validates and data is 
-    return {"NEW POST"}           # available via 'new_post'. If false, then error is sent back to
-                                  # user stating where the error is. AUTOMATIC VALIDATION
+@app.post("/posts")
+def create_posts(post: Post): # FastAPI automatically checks frontend payload if data fits the 
+    post_dict = post.dict()   # schema model, Post. If true, then it validates and data is
+    post_dict['id'] = randrange(0, 10000000) # available via 'new_post'. If false, then error is sent back to
+    my_posts.append(post_dict)                        # user stating where the error is. AUTOMATIC VALIDATION
+    return {"data": post_dict}           
+                                  
+
+@app.get("/posts/{id}")
+def get_post(id: int):           # Validates that path parameter can be turned 
+    post = find_post(id)         # into int and does so if true. Now throws error 
+    return{"post_detail": post}  # if the parameter is not the selcted type of  
+                                 # int. Also, now the frontend has a good way of 
+                                 # understanding what they did wrong
+
 
