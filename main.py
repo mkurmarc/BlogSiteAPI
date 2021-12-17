18a48a1663b44c8b9f0352779ef99e9c79ae5803
mkurmarc <51379562+mkurmarc@users.noreply.github.com>
2021-12-16 16:26:20 -0800

    add: delete and update route

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
@@ -24,6 +23,10 @@ def find_post(id):
         if p["id"] == id:
             return p
 
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
 
 @app.get("/")
 def root():
@@ -43,7 +46,7 @@ def create_posts(payload: dict = Body(...)): # saves body content to a dict name
     return {"new_post": f"title {payload['title']} content: {payload['content']}"}
 '''
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post): # FastAPI automatically checks frontend payload if data fits the 
     post_dict = post.dict()   # schema model, Post. If true, then it validates and data is
     post_dict['id'] = randrange(0, 10000000) # available via 'new_post'. If false, then error is sent back to
@@ -52,10 +55,35 @@ def create_posts(post: Post): # FastAPI automatically checks frontend payload if
                                   
 
 @app.get("/posts/{id}")
-def get_post(id: int):           # Validates that path parameter can be turned 
-    post = find_post(id)         # into int and does so if true. Now throws error 
-    return{"post_detail": post}  # if the parameter is not the selcted type of  
-                                 # int. Also, now the frontend has a good way of 
-                                 # understanding what they did wrong
+def get_post(id: int):    # 'id: int' Validates that path parameter can be turned into int and does           
+    post = find_post(id)  # so if true. Now throws error if the parameter is not the selcted type of
+    if not post:          # int. Also, now the frontend has a good way of understanding what they did wrong
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"post with id: {id} was not found") 
+    return{"post_detail": post}            
+                                    
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id {id} does not exist")
+
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)                                   
 
 
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post): # 'post: Post' makes sures the request comes in with the right schema
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id {id} does not exist")
+    
+    post_dict = post.dict()
+    post_dict['id'] = id 
+    my_posts[index] = post_dict
+    return {'data': post_dict}
