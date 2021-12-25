3f3d2603c4080b7b41c9c23db3d7b5ca0ff7c737
mkurmarc <51379562+mkurmarc@users.noreply.github.com>
2021-12-24 23:48:30 -0800

    add(post): create post now saves to db

--- app/main.py
+++ app/main.py
@@ -54,12 +54,10 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    cursor.execute("""
-        SELECT * FROM posts
-    """)
+    cursor.execute(""" SELECT * FROM posts """)
     posts = cursor.fetchall()
     return{"data": posts} # If I pass in an array like this, FastAPI 
-                             # serializes 'my_posts' converting it into JSON
+                          # serializes 'my_posts' converting it into JSON
 
 '''
 # another example of using the Body
@@ -68,13 +66,24 @@ def create_posts(payload: dict = Body(...)): # saves body content to a dict name
     print(payload)
     return {"new_post": f"title {payload['title']} content: {payload['content']}"}
 '''
-
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post): # FastAPI automatically checks frontend payload if data fits the 
-    post_dict = post.dict()   # schema model, Post. If true, then it validates and data is
-    post_dict['id'] = randrange(0, 10000000) # available via 'new_post'. If false, then error is sent back to
-    my_posts.append(post_dict)                        # user stating where the error is. AUTOMATIC VALIDATION
-    return {"data": post_dict}           
+'''
+#1
+FastAPI automatically checks frontend payload if data fits the schema model, 'Post'. If true,
+then it validates and data is available via 'new_post'. If false, then error is sent back to
+user stating where the error is. AUTOMATIC VALIDATION.
+#2
+Use this style of string witht he % symbols because it protects against SQL injection attacks
+'''
+  
+@app.post("/posts", status_code=status.HTTP_201_CREATED) 
+def create_posts(post: Post): #1 
+    cursor.execute( #2          
+        """ INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, 
+        (post.title, post.content, post.published) 
+    ) 
+    new_post = cursor.fetchone()
+    conn.commit() # pushes the changes out to the database
+    return {"data": "created post"}           
                                   
 
 @app.get("/posts/{id}")
