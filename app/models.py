4568acd1c802db75ca419f780529f2fb658669fc
mkurmarc <51379562+mkurmarc@users.noreply.github.com>
2021-12-31 19:54:13 -0800

    add(db_column): owner_id column added in Post model to link User and Post tables

--- app/models.py
+++ app/models.py
@@ -1,16 +1,17 @@
 from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.schema import ForeignKey
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from .database import Base
 
 class Post(Base):
     __tablename__ = "posts"
-
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default='TRUE', nullable=True)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
 
 
 class User(Base):
