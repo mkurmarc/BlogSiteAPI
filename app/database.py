25e8458110e78ac5968e65a0352fb2d6218af123
mkurmarc <51379562+mkurmarc@users.noreply.github.com>
2022-02-13 12:36:53 -0800

    refactor(): utilize model for env variables instead of .env directly

--- app/database.py
+++ app/database.py
@@ -4,9 +4,10 @@ from sqlalchemy.orm import sessionmaker
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
+from .config import settings
 
 # 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:***REMOVED***@localhost/BlogSiteAPI'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 #                         'postgresql://<username>:<password>@<ip-address/hostname>/<database_name>'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
@@ -27,7 +28,7 @@ def get_db():
 while True:
     try:
         conn = psycopg2.connect(host='localhost', database='BlogSiteAPI', user='postgres', 
-                                password='***REMOVED***', cursor_factory=RealDictCursor)
+                                password='Zergling22!', cursor_factory=RealDictCursor)
         cursor = conn.cursor()
         print("DB connection was successful!")
         break
--- app/oauth2.py
+++ app/oauth2.py
@@ -4,6 +4,7 @@ from . import schemas, database, models
 from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
+from .config import settings
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login') # the path api for login
@@ -11,9 +12,9 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login') # the path api for login
 # SECRET_KEY
 # Algorithm
 # Expiration time of the token, aka how long the user can be logged in
-SECRET_KEY = "***REMOVED***"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+SECRET_KEY = settings.secret_key
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
 
 
 def create_access_token(data: dict):
