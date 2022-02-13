20523f8cd67f43f96be2e27a07853df2428dfe17
mkurmarc <51379562+mkurmarc@users.noreply.github.com>
2022-02-12 19:45:31 -0800

    build(): config.py to use .env file and validation with pydantic

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 from .routers import post, user, auth
+from .config import settings
 
 #Create the database tables; In a very simplistic way create the database tables:
 models.Base.metadata.create_all(bind=engine)
